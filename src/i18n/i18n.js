dd39ba89b464706b0a87a0981760a18d997d4891
KostadinovaMitra <mitra.kostadinova@pcg.io>
2025-05-13 01:29:36 +0200

    Update Language

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -16,40 +16,16 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: "mk", // Default language
+    fallbackLng: "mk", // Set Macedonian as default
     detection: {
       order: ["localStorage", "navigator"],
       caches: ["localStorage"],
+      lookupLocalStorage: "i18nextLng",
     },
     interpolation: {
       escapeValue: false,
     },
+    supportedLngs: ["mk", "en"], // List supported languages
   });
 
 export default i18n;
-
-// // i18n.js
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
-
-// import translationMK from './locales/mk/translation.json';
-// import translationEN from './locales/en/translation.json';
-
-// const resources = {
-//   mk: { translation: translationMK },
-//   en: { translation: translationEN }
-// };
-
-// i18n
-//   .use(LanguageDetector)
-//   .use(initReactI18next)
-//   .init({
-//     resources,
-//     fallbackLng: 'mk',
-//     interpolation: {
-//       escapeValue: false
-//     }
-//   });
-
-// export default i18n;
